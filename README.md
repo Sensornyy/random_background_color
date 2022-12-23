6fef1123cff6edc0febf2d85fea5c6ada343d824
Sensornyy <maksym.koriak@chisw.com>
2022-12-23 17:28:14 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# random_background_color
+
+Single screen app that dynamically changes Scaffold background color by algorithm which returns random color.
---
+++ android/app/src/main/kotlin/com/example/random_background_color/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.random_background_color
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
